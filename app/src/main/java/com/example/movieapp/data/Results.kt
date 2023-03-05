03161af1314185135c29d7dd1f95eedd37ae9657
andre <andreluiz.7391@gmail.com>
2023-03-05 18:28:06 -0400

    Corrigido o formato da data no MovieList.

--- app/src/main/java/com/example/movieapp/data/Results.kt
+++ app/src/main/java/com/example/movieapp/data/Results.kt
@@ -2,6 +2,8 @@ package com.example.movieapp.data
 
 import com.example.movieapp.ApiCredentials
 import com.squareup.moshi.JsonClass
+import java.text.SimpleDateFormat
+import java.util.*
 
 @JsonClass(generateAdapter = true)
 data class Results(
@@ -12,4 +14,20 @@ data class Results(
     val title: String?
 ){
     fun getImageUrl() = ApiCredentials.imageUrl + poster_path
+    val dateFormatted = formatDate(release_date)
+
+    private fun formatDate(date: String?): String? {
+        val dateFormatted: String?
+        val DATE_PATTERN = "yyyy-MM-dd"
+        val DATE_PATTERN_CARD = "dd/MM/yyyy"
+        val dateFormartter = SimpleDateFormat(DATE_PATTERN, Locale.getDefault())
+        val dateCardFormatter = SimpleDateFormat(DATE_PATTERN_CARD, Locale.getDefault())
+
+        dateFormatted = dateFormartter.parse(date)?.let { date ->
+            dateCardFormatter.format(date)
+        }
+
+        return dateFormatted
+
+    }
 }
