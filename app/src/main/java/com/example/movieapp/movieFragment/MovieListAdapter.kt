a57427d3b9c9964a99e6a0336cc736b7eecb30f3
andre <andreluiz.7391@gmail.com>
2023-03-12 16:33:27 -0400

    Ajustadando Adapter e Views.

--- app/src/main/java/com/example/movieapp/movieFragment/MovieListAdapter.kt
+++ app/src/main/java/com/example/movieapp/movieFragment/MovieListAdapter.kt
@@ -7,17 +7,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.movieapp.data.Results
 import com.example.movieapp.databinding.FragmentMovieItemBinding
 
-//interface MovieItemListener{
-//    fun onItemSelected(position: Int)
-//}
+interface MovieItemListener {
+    fun onItemSelected(position: Int)
+}
 
-class MovieListAdapter() : RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
-
-    var onClick : ((Int) -> Unit)? = null
+class MovieListAdapter(private val listener: MovieItemListener) : RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
 
     private var values: List<Results> = ArrayList()
 
-    fun updateData(movieList: List<Results>){
+    fun updateData(movieList: List<Results>) {
         values = movieList
         notifyDataSetChanged()
     }
@@ -37,27 +35,20 @@ class MovieListAdapter() : RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.bindItem(item)
-        holder.view.setOnClickListener {
-            onClick?.let {
-                it(item.id)
-            }
-//            listener.onItemSelected(position)
+        holder.view.setOnClickListener{
+            listener.onItemSelected(position)
         }
 
     }
 
     override fun getItemCount(): Int = values.size
 
-    fun onMovieClick(listener: (Int)-> Unit){
-        onClick = listener
-    }
-
     inner class ViewHolder(private val binding: FragmentMovieItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         val view: View = binding.root
 
-        fun bindItem(item: Results){
+        fun bindItem(item: Results) {
             binding.movieItem = item
             binding.executePendingBindings()
         }
